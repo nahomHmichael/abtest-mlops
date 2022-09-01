2a5d7af868ac1c478df4ca416376c14a7bf29eed
yonamg <softeng2006@gmail.com>
2022-09-01 23:19:52 +0300

    prepare reusable python class

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,97 +1,77 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "d78a055e",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import json\n",
-    "import pandas as pd\n",
-    "\n",
-    "class LoadData:\n",
-    "    \"\"\"\n",
-    "    this class contains functions to load data from json, excel or csv files into a pandas dataframe \n",
-    "    \n",
-    "    Return\n",
-    "    ------\n",
-    "    dataframe\n",
-    "    \"\"\"\n",
-    "    \n",
-    "    def __init__(self):\n",
-    "        pass\n",
-    "    \n",
-    "    def read_json(self, json_file: str, dfExtractFunc: object )->pd.DataFrame:\n",
-    "        \"\"\"\n",
-    "        json file reader to open and read json files into a panda dataframe\n",
-    "        Args:\n",
-    "        -----\n",
-    "        json_file: str - path of a json file\n",
-    "        dfExtractFunc: object - The function that will used to extract data from the list formed from the json file\n",
-    "        Returns\n",
-    "        -------\n",
-    "        dataframe containing data extracted from the json file\n",
-    "        \"\"\"\n",
-    "\n",
-    "        data_list = []\n",
-    "        for item in open(json_file,'r'):\n",
-    "            data_list.append(json.loads(item))\n",
-    "\n",
-    "        df = dfExtractFunc(data_list)\n",
-    "        return df\n",
-    "    \n",
-    "    def read_excel(self, excel_file, startRow=0)->pd.DataFrame: \n",
-    "        \"\"\"\n",
-    "        excel file reader to open and read excel files into a panda dataframe\n",
-    "        Args:\n",
-    "        -----\n",
-    "        excel_file: str - path of a excel file\n",
-    "        engine: str - sets the default engine used by pandas to load the excel file\n",
-    "        startRow: int - sets the first row in excel sheet where pandas should start loading from\n",
-    "        Returns\n",
-    "        -------\n",
-    "        dataframe containing data extracted from the excel file\n",
-    "        \"\"\"\n",
-    "        \n",
-    "        df = pd.read_excel(excel_file,engine='openpyxl')\n",
-    "        return df\n",
-    "    \n",
-    "    def read_csv(self, csv_file)->pd.DataFrame:\n",
-    "        \"\"\"\n",
-    "        csv file reader to open and read csv files into a panda dataframe\n",
-    "        Args:\n",
-    "        -----\n",
-    "        csv_file: str - path of a json file\n",
-    "        Returns\n",
-    "        -------\n",
-    "        dataframe containing data extracted from the csv file        \n",
-    "        \n",
-    "        \"\"\"\n",
-    "        \n",
-    "        return pd.read_csv(csv_file)"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.12"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import json
+import pandas as pd
+
+class LoadData:
+    """
+    this class contains functions to load data from json, excel or csv files into a pandas dataframe 
+    
+    Return
+    ------
+    dataframe
+    """
+    
+    def __init__(self):
+        pass
+    
+    def read_json(self, json_file: str, dfExtractFunc: object )->pd.DataFrame:
+        """
+        json file reader to open and read json files into a panda dataframe
+        Args:
+        -----
+        json_file: str - path of a json file
+        dfExtractFunc: object - The function that will used to extract data from the list formed from the json file
+        Returns
+        -------
+        dataframe containing data extracted from the json file
+        """
+
+        data_list = []
+        for item in open(json_file,'r'):
+            data_list.append(json.loads(item))
+
+        df = dfExtractFunc(data_list)
+        return df
+    
+    def read_excel(self, excel_file, startRow=0)->pd.DataFrame: 
+        """
+        excel file reader to open and read excel files into a panda dataframe
+        Args:
+        -----
+        excel_file: str - path of a excel file
+        engine: str - sets the default engine used by pandas to load the excel file
+        startRow: int - sets the first row in excel sheet where pandas should start loading from
+        Returns
+        -------
+        dataframe containing data extracted from the excel file
+        """
+        
+        df = pd.read_excel(excel_file,engine='openpyxl')
+        return df
+    
+    def read_csv(self, csv_file)->pd.DataFrame:
+        """
+        csv file reader to open and read csv files into a panda dataframe
+        Args:
+        -----
+        csv_file: str - path of a json file
+        Returns
+        -------
+        dataframe containing data extracted from the csv file        
+        
+        """
+        
+        return pd.read_csv(csv_file)
+            
+
+
+# In[ ]:
+
+
+
+
